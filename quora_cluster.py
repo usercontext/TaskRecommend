d746956ea116a1e782ac6f481fcb183c63301391
Sebastin Santy <sebastinssanty@gmail.com>
2018-06-26 12:23:08 +0100

    New tree with personal and reserve list

--- quora_cluster.py
+++ quora_cluster.py
@@ -22,7 +22,38 @@ def cosine_similarity(vector1, vector2):
 
 personal_threshold = 0.8
 
-def quora(tree, ques, doc, agg_score=0, relation=[]):
+def likelihood(exude, collapse, temp3):
+    return random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)
+
+def new_node_child(tree, ques, op):
+    if "children" not in tree:
+        tree["children"] = []
+        ques["children"] = []
+    no_of_children = len(tree["children"])
+    net_embedding = np.zeros(300)
+    for key, value in op.items():
+        vector_sum = np.zeros(300)
+        for k in value:
+            question, tags = k.split('\t')
+            tags = re.split('\| | \| |\n', tags)
+            tag_list = list(filter(None, tags))
+            if len(tag_list) > 0:
+                doc = nlp(' '.join(tag_list))
+                vector_sum += doc.vector
+        vector_sum = np.divide(vector_sum, len(value))
+        net_embedding += vector_sum
+        tree["children"].append({"name": "anonymoustag", "reserve": value, "embedding": vector_sum.tolist()})
+        ques["children"].append({"name": "anonymoustag", "reserve": value })
+    net_embedding = np.divide(net_embedding, len(op))
+    #Accounting for leaves
+    if len(tree["children"]) == len(op):
+        tree["embedding"] = net_embedding.tolist()
+    else:
+        divfac = len(op) / len(tree["children"])
+        new_embedding = np.asarray([(1-divfac)*i for i in tree["embedding"]]) + np.asarray([(divfac)*i for i in net_embedding.tolist()])
+        tree["embedding"] = new_embedding.tolist()
+
+def quora(tree, ques, doc, prev_score=0, relation=[]):
     score = {}
     if "children" in tree:
         for child in tree["children"]:
@@ -35,12 +66,12 @@ def quora(tree, ques, doc, agg_score=0, relation=[]):
             score[child["name"]] = loc_score
         score = sorted(score.items(), key=operator.itemgetter(1), reverse=True)
         # pp.pprint(score)
-        if score[0][1] > agg_score:
+        if score[0][1] > prev_score*0.95:
             relation.append(score[0][0])
-            agg_score += score[0][1]
+            prev_score = score[0][1]
             new_tree = (item for item in tree["children"] if item["name"] == score[0][0]).__next__()
             new_ques_tree = (item for item in ques["children"] if item["name"] == score[0][0]).__next__()
-            quora(new_tree, new_ques_tree, doc, agg_score, relation)
+            quora(new_tree, new_ques_tree, doc, prev_score, relation)
         elif cosine_similarity(doc.vector, tree["personal_embed"]) > personal_threshold:
             if "personal_list" not in ques:
                 ques["personal_list"] = []
@@ -79,7 +110,7 @@ for i in range(len(data)):
         print(i, '\t', question, '\t', related)
         # exit()
         if not i % 100:
-            with open('tree_without_brt.json', 'w') as outfile:
+            with open('tree_without_brt1.json', 'w') as outfile:
                 import re
                 output = json.dumps(ques, indent=4)
                 outfile.write(re.sub(r'(?<=\d),\s+', ', ', output))
